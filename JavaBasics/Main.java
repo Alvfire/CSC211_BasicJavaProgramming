4b83fbe7a7bd4af52277c9fab87555f59c817e33
Alvfire <pelaar@farmingdale.edu>
2024-02-01 00:14:51 -0500

    Conditional Statements

--- JavaBasics/Main.java
+++ JavaBasics/Main.java
@@ -59,6 +59,22 @@ public class Main {
             {
                 System.out.println("You're not eligible");
             }
+
+        // Switch case:
+        //Multi-choice decision using switch-case
+
+        int day = 3;
+            switch (day) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                    System.out.println("Tuesday");
+                    break;
+               //...
+                default:
+                    System.out.println("Invalid day");
+            }
         }
     }
 
