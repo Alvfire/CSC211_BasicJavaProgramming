82d78d6e0650c71358f7af696e938bcc5a77ef54
Alvfire <pelaar@farmingdale.edu>
2024-01-31 23:28:37 -0500

    Added variables

---
+++ JavaBasics/Main.java
@@ -0,0 +1,10 @@
+package JavaBasics;
+
+public class Main {
+    public static void main(String[] args) {
+        //Variables
+        int age = 25;
+        double salary = 50000.0;
+        String name = "John";
+    }
+}
